d105d6b2af91b6bca169621daf2622a477d43eb3
Easy1229 <2859412930@qq.com>
2023-01-05 14:58:53 +0800

    Update

--- Assets/EditorExtension/Editor/002.MenuItemOpenFile/OpenFile.cs
+++ Assets/EditorExtension/Editor/002.MenuItemOpenFile/OpenFile.cs
@@ -5,13 +5,13 @@ namespace EditorExtension
 {
     public class OpenFile
     {
-        [MenuItem("EditorExtension/001.MenuItem/OpenFile")]
+        [MenuItem("EditorExtension/002.MenuItemOpenFile/OpenFile")]
         private static void OpenDataFile()
         {
             EditorUtility.RevealInFinder(Application.persistentDataPath);
         }
         
-        [MenuItem("EditorExtension/001.MenuItem/OpenLocalFile")]
+        [MenuItem("EditorExtension/002.MenuItemOpenFile/OpenLocalFile")]
         private static void OpenLocalFile()
         {
             EditorUtility.RevealInFinder(Application.dataPath.Replace("Assets","Doc"));
